a44e01aa0ff223d2bd630eb538ebc07eb353f8c3
Arif1188 <edinlink92@gmail.com>
2025-10-26 10:55:43 +0500

    Final section of Bridge pattern

--- BridgePattern.py
+++ BridgePattern.py
@@ -39,3 +39,15 @@ class EmailSender(ISender):
     def send(self):
         self.reader.read()
         print("Sent to Email")
+        
+        
+if __name__ == '__main__':
+    db_reader = DBReader()
+    file_reader = FileReader()
+    html_reader = HTMLReader()
+
+    telegram_sender = TelegramSender(html_reader)
+    email_sender = EmailSender(html_reader)
+
+    telegram_sender.send()
+    email_sender.send()
